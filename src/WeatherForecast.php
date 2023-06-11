94af6276a889370c789915d4f7b72418b3bcd911
DragonDev1010 <provic0099@gmail.com>
2023-06-10 21:24:04 -0400

    add configure for api key

--- src/WeatherForecast.php
+++ src/WeatherForecast.php
@@ -4,8 +4,20 @@ namespace Great\Weatherforecast;
 use Great\Weatherforecast\Models\Iplocation;
 
 class WeatherForecast {
-  public static function getLocation() {
-    return 'get location';
+  public static function getLocation($ipAddress) {
+    $apiKey = config('weatherForecast.api_key');
+    return $apiKey;
+    // $lang = "en";
+    // $fields = "*";
+    // $excludes = "";
+
+    // $url = "https://api.ipgeolocation.io/ipgeo?apiKey=" . $apiKey . "&ip=" . $ipAddress . "&lang=" . $lang . "&fields=" . $fields . "&excludes=" . $excludes;
+
+    // $response = Http::get($url);
+    // $decoded_data = json_decode($response->getBody());
+    // $latitude = $decoded_data->latitude;
+    // $longitude = $decoded_data->longitude;
+    // return [$latitude, $longitude];
   }
 
   public static function insertIplocationTable() {
