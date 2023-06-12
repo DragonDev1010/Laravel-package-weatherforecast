c913e814b08538254d12fe76b4fd5192533ab5ee
DragonDev1010 <provic0099@gmail.com>
2023-06-12 08:12:36 -0400

    fix form post route

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,4 +5,4 @@ use Great\Weatherforecast\WeatherForecast;
 
 Route::get('/weather-forecast', [WeatherForecast::class, 'index']);
 Route::get('/weather-forecast/{ipaddress}', [WeatherForecast::class, 'getLocation']);
-Route::post('get-weather-forecast', [WeatherForecast::class, 'getWeatherForecast'])->name('getWeatherForecast');
+Route::post('/get-weather-forecast', [WeatherForecast::class, 'getWeatherForecast'])->name('getWeatherForecast');
