b94a4b27a21d3dac47482e8e46b4b02c88912154
DragonDev1010 <provic0099@gmail.com>
2023-06-12 08:05:46 -0400

    add get_weather_forecast function and route

--- src/WeatherForecast.php
+++ src/WeatherForecast.php
@@ -32,4 +32,38 @@ class WeatherForecast {
     $new_iplocation->save();
     return true;
   }
+
+  public function getWeatherForecast($ipAddress, $startDate, $server) {
+    // Request : {$ipaddress, $startDate, $forecastService}
+    // Get `latitude` and `longitude` with $ipAddress
+    list($latitude, $longitude) = $this->getLocationByIp($ipAddress);
+
+    // when user does not select date, it is defined as the current date.
+    if ($startDate === null)
+      $startDate = Carbon::now()->format('Y-m-d');
+    
+    //  get the end date that is 5 days after a given start date
+    $endDate = Carbon::parse($startDate)->addDays(4)->format('Y-m-d');
+
+    switch ($server) {
+      case 'dwd-icon':
+        // Get forecast api url with $latitude, $longitude, $startDate, $endDate
+        $url = 'https://api.open-meteo.com/v1/dwd-icon?latitude='.$latitude.'&longitude='.$longitude.'&hourly=temperature_2m&start_date='.$startDate.'&end_date='.$endDate;
+        break;
+        
+      default:
+        $url = 'https://api.open-meteo.com/v1/gfs?latitude='.$latitude.'&longitude='.$longitude.'&hourly=temperature_2m&start_date='.$startDate.'&end_date='.$endDate;
+        break;
+    }
+    return $this->_getWeather($url);
+  }
+
+  private function _getWeather($url) {
+    $response = Http::get($url);
+    $decoded_data = json_decode($response->getBody());
+
+    $forecast_time = $decoded_data->hourly->time;
+    $forecast_temperature = $decoded_data->hourly->temperature_2m;
+    return [$forecast_time, $forecast_temperature];
+  }
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Great\Weatherforecast\WeatherForecast;
 
-Route::get('/weather-forecast/{ipaddress}', [WeatherForecast::class, 'getLocation']);
-
 Route::get('/weather-forecast', [WeatherForecast::class, 'index']);
+Route::get('/weather-forecast/{ipaddress}', [WeatherForecast::class, 'getLocation']);
+Route::post('get-weather-forecast', [WeatherForecast::class, 'get_weather_forecast'])->name('get-weather-forecast');
