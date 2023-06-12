559a80c2096f67659fdf05e3adb46a4a5cd0971d
DragonDev1010 <provic0099@gmail.com>
2023-06-12 07:43:38 -0400

    update getLocation function

--- src/WeatherForecast.php
+++ src/WeatherForecast.php
@@ -6,19 +6,18 @@ use Great\Weatherforecast\Models\Iplocation;
 
 class WeatherForecast {
   public static function getLocation($ipAddress) {
-    // $apiKey = config('weatherForecast.api_key');
-    // $lang = "en";
-    // $fields = "*";
-    // $excludes = "";
+    $apiKey = config('weatherForecast.api_key');
+    $lang = "en";
+    $fields = "*";
+    $excludes = "";
 
-    // $url = "https://api.ipgeolocation.io/ipgeo?apiKey=" . $apiKey . "&ip=" . $ipAddress . "&lang=" . $lang . "&fields=" . $fields . "&excludes=" . $excludes;
+    $url = "https://api.ipgeolocation.io/ipgeo?apiKey=" . $apiKey . "&ip=" . $ipAddress . "&lang=" . $lang . "&fields=" . $fields . "&excludes=" . $excludes;
 
-    // $response = Http::get($url);
-    // $decoded_data = json_decode($response->getBody());
-    // $latitude = $decoded_data->latitude;
-    // $longitude = $decoded_data->longitude;
-    // return [$latitude, $longitude];
-    return 'test';
+    $response = Http::get($url);
+    $decoded_data = json_decode($response->getBody());
+    $latitude = $decoded_data->latitude;
+    $longitude = $decoded_data->longitude;
+    return [$latitude, $longitude];
   }
 
   public static function insertIplocationTable() {
